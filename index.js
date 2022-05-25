88d90269fa0cd895c6934c300f524d511e275db5
Matthew Holung <matthewholung@Matthews-MacBook-Pro.local>
2022-05-25 01:40:09 -0400

    Running on DigitalOcean droplet

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/account/create/:name/:email/:password', function (req, res) {
     // else create user
     dal.create(req.params.name, req.params.email, req.params.password)
         .then((user) =>{
-        console.log(user);
+        //console.log(user);
         res.send(user);
     });
 });
@@ -31,7 +31,7 @@ app.get('/account/userdata/:email/:password', function (req, res) {
 app.get('/account/all', function (req, res) {
     dal.all().
         then((docs) => {
-        console.log(docs);
+        //console.log(docs);
         res.send(docs);
     });
 });
@@ -69,6 +69,6 @@ app.get('/acount/destroy/:email',function (req, res){
     })
 })
 
-var port = 3000;
+var port = 8080;
 app.listen(port);
 console.log(`Running on port: ${port}`);
